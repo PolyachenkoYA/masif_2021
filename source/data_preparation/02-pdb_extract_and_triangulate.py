43e29e2401edbcaf38628f203c6d7c56ed75a44f
Yura <polyachenkoya@gmail.com>
2021-07-22 18:13:27 +0200

    main center_types removed

--- source/data_preparation/02-pdb_extract_and_triangulate.py
+++ source/data_preparation/02-pdb_extract_and_triangulate.py
@@ -48,33 +48,32 @@ def interpolate_3Dpoints(crds, N):
 
 def get_lin_features_from_vertices(vertices, center_coords, vertices_features, Ravg):
 	# compute features averaged from vertices around the points in the center_coords
-	N_center_types = center_coords.shape[0]
-	N_resd = center_coords.shape[1]
+	N_resd = center_coords.shape[0]
 	N_feat = vertices_features.shape[0]
-	lin_feauture_sq = np.zeros((N_center_types, N_resd, N_feat))
-	lin_feauture_sq_u = np.zeros((N_center_types, N_resd, N_feat))
-	for center_type_i in range(N_center_types):
-		for res_i in range(N_resd):
-			crds = vertices - center_coords[center_type_i, res_i, :]
-			dists = np.linalg.norm(crds, axis=1)
-			w = np.exp( - np.power(dists, 2) / (2 * Ravg**2))   # Ravg=0.23 is optimal here
-			#w = np.heaviside(Ravg - dists, 0)  # Ravg = 0.43 is optimal here
-			for f_i in range(N_feat):
-				lin_feauture_sq[center_type_i, res_i, f_i] = np.sum(vertices_features[f_i, :] * w)
+	lin_feauture_sq = np.zeros((N_resd, N_feat))
+	lin_feauture_sq_u = np.zeros((N_resd, N_feat))
+	for res_i in range(N_resd):
+		crds = vertices - center_coords[res_i, :]
+		dists = np.linalg.norm(crds, axis=1)
+		w = np.exp( - np.power(dists, 2) / (2 * Ravg**2))   # Ravg=0.23 is optimal here
+		#w = np.heaviside(Ravg - dists, 0)  # Ravg = 0.43 is optimal here
 		for f_i in range(N_feat):
-			lin_feauture_sq_u[center_type_i, :, f_i] = my.unitize(lin_feauture_sq[center_type_i, :, f_i])
+			lin_feauture_sq[res_i, f_i] = np.sum(vertices_features[f_i, :] * w)
+
+	for f_i in range(N_feat):
+		lin_feauture_sq_u[:, f_i] = my.unitize(lin_feauture_sq[:, f_i])
 
 	return lin_feauture_sq_u
 
-def get_R_cor_sq(feature, main_mode):
+def get_R_cor_sq(feature):
 	# compute pairwise R-corellation
-	N_feat = feature.shape[2]
+	N_feat = feature.shape[1]
 	R_cor = np.zeros((N_feat, N_feat))
 	for f1_i in range(N_feat):
 		R_cor[f1_i, f1_i] = 1
 		for f2_i in range(f1_i + 1, N_feat):
 			#print(scipy.stats.pearsonr(lin_feauture_sq_u[main_mode, :, f1_i], lin_feauture_sq_u[main_mode, :, f2_i]))
-			R_cor[f1_i, f2_i], _ = scipy.stats.pearsonr(feature[main_mode, :, f1_i], feature[main_mode, :, f2_i])
+			R_cor[f1_i, f2_i], _ = scipy.stats.pearsonr(feature[:, f1_i], feature[:, f2_i])
 			R_cor[f2_i, f1_i] = R_cor[f1_i, f2_i]
 
 	return R_cor
@@ -82,7 +81,7 @@ def get_R_cor_sq(feature, main_mode):
 def main():
 	center_coords_names = ['CA centers', 'center-of-mass positions', 'backbone']
 	main_mode = 0
-	N_center_types = len(center_coords_names)
+	#N_center_types = len(center_coords_names)
 
 	N_interp_scale = 5
 	N_R = 20  # log space
@@ -130,23 +129,18 @@ def main():
 	### ======================== load molecule ============================
 	molecule = md.load_pdb(pdb_filepath)
 	N_resd = molecule.topology.n_residues
-	#center_coords_residues = np.zeros((N_center_types, N_resd, 3))
-	center_coords_residues = []
-	center_coords_residues.append(molecule.xyz[0, molecule.topology.select('name CA'), :])   # C_A coords
-	center_coords_residues.append(np.zeros((N_resd, 3)))   # center of mass coords
-	for res_i in range(N_resd):   # center of mass coords
-		center_coords_residues[1][res_i, :] = np.mean(molecule.xyz[0, molecule.topology.select('resid == ' + str(res_i)), :], axis=0)   # center of mass coords
-	center_coords_residues.append(molecule.xyz[0, molecule.topology.select('backbone'), :])   # backbone_coords
-
+	if(main_mode == 0):
+			center_coords_residues = molecule.xyz[0, molecule.topology.select('name CA'), :]   # C_A coords
+	elif(main_mode == 1):
+			center_coords_residues = np.zeros((N_resd, 3))   # center of mass coords
+			for res_i in range(N_resd):   # center of mass coords
+				center_coords_residues[res_i, :] = np.mean(molecule.xyz[0, molecule.topology.select('resid == ' + str(res_i)), :], axis=0)   # center of mass coords
+	elif(main_mode == 2):
+			center_coords_residues = molecule.xyz[0, molecule.topology.select('backbone'), :]   # backbone_coords
 	### =================== construct paths =======================
 	N_centers = N_resd * N_interp_scale
-	center_coords = np.zeros((N_center_types, N_centers, 3))
-	chain_vectors = []
-	center_resd_indices = np.zeros((N_center_types, N_centers), dtype=int)
-	for center_type_i in range(N_center_types):
-		center_coords[center_type_i, :, :], center_resd_indices[center_type_i, :], chain_vectors_local = \
-			interpolate_3Dpoints(center_coords_residues[center_type_i], N_centers)
-		chain_vectors.append(chain_vectors_local)
+	center_coords, center_resd_indices, chain_vectors = \
+		interpolate_3Dpoints(center_coords_residues, N_centers)
 
 	### ==================== construct the mesh and compute features.===================
 	regular_mesh, vertex_normals, vertices, names = \
@@ -163,29 +157,31 @@ def main():
 	cut_mesh = meshcut.TriangleMesh(vertices, regular_mesh.faces)
 	sections_3D = []
 	sections_2D = []
-	sections_to_3D = np.zeros((N_center_types, N_centers, 4, 4))
+	center_crds_2D = np.zeros((2, N_centers))
+	sections_to_3D = np.zeros((N_centers, 4, 4))
 	section_normals = np.zeros((3, N_centers))
-	for center_type_i in range(N_center_types):
-		section_normals[:, 0] = chain_vectors[center_type_i][center_resd_indices[center_type_i, 0] - 1, :]
-		center_resd_indices[center_type_i, 0]
-		for cent_i in range(N_centers):
-			chain_vectors_to_average = chain_vectors[center_type_i][max(center_resd_indices[center_type_i, cent_i] - 1 - chain_vec_avg, 0) : \
-								                                    min(center_resd_indices[center_type_i, cent_i] - 1 + chain_vec_avg, chain_vectors[center_type_i].shape[0] - 1), :]
-			#section_normals[:, cent_i] = np.main(chain_vectors_to_average / np.linalg.norm(chain_vectors_to_average, axis=), axis=0)
-			section_normals[:, cent_i] = np.sum(chain_vectors_to_average, axis=0)
-			section = meshcut.cross_section_mesh(cut_mesh, meshcut.Plane(center_coords[center_type_i, cent_i, :], \
-												                         section_normals[:, cent_i]))   # trimesh does not separate closed subsections, this does, tho it works much slover
-			_, sections_to_3D[center_type_i, cent_i, :, :] = \
-				regular_trimesh.section(plane_origin=center_coords[center_type_i, cent_i, :], \
-								        plane_normal=section_normals[:, cent_i]).to_planar(check=False)
+	for cent_i in range(N_centers):
+		chain_vectors_to_average = chain_vectors[max(center_resd_indices[cent_i] - 1 - chain_vec_avg, 0) : \
+							                     min(center_resd_indices[cent_i] - 1 + chain_vec_avg, chain_vectors.shape[0] - 1), :]
+		#section_normals[:, cent_i] = np.main(chain_vectors_to_average / np.linalg.norm(chain_vectors_to_average, axis=), axis=0)
+		section_normals[:, cent_i] = np.sum(chain_vectors_to_average, axis=0)
+		section = meshcut.cross_section_mesh(cut_mesh, meshcut.Plane(center_coords[cent_i, :], \
+											                         section_normals[:, cent_i]))   # trimesh does not separate closed subsections, this does, tho it works much slover
+		_, sections_to_3D[cent_i, :, :] = \
+			regular_trimesh.section(plane_origin=center_coords[cent_i, :], \
+							        plane_normal=section_normals[:, cent_i]).to_planar(check=False)
 
-			sections_2D.append([])
-			for subsection_id in range(len(section)):
-				section[subsection_id] = section[subsection_id].T
-				subsection_coord_augmented = np.concatenate((section[subsection_id], np.ones((1, section[subsection_id].shape[1]))), axis=0)
-				subsection_2Dcoord = np.linalg.solve(sections_to_3D[main_mode, cent_i, :, :], subsection_coord_augmented)[0:2, :]
-				sections_2D[-1].append(subsection_2Dcoord)
-			sections_3D.append(section)
+		center_crds_2D[:, cent_i] = np.linalg.solve(sections_to_3D[cent_i, :, :], \
+											         np.append(center_coords[cent_i, :], 1).T)[0:2]
+		sections_2D.append([])
+		for subsection_id in range(len(section)):
+			section[subsection_id] = section[subsection_id].T
+			subsection_coord_augmented = np.concatenate((section[subsection_id], \
+											             np.ones((1, section[subsection_id].shape[1]))), axis=0)
+			subsection_2Dcoord = np.linalg.solve(sections_to_3D[cent_i, :, :], \
+									             subsection_coord_augmented)[0:2, :]
+			sections_2D[-1].append(subsection_2Dcoord)
+		sections_3D.append(section)
 
 	lin_feauture_sq_u = lin_vertices_feautures
 
@@ -193,21 +189,18 @@ def main():
 	R_cor = np.zeros((N_R, N_feat, N_feat))
 	R_arr = np.power(R_log_base, np.linspace(0, np.log(Rmax / Rmin) / np.log(R_log_base), N_R)) * Rmin
 	for R_i in range(N_R):
-		R_cor[R_i, :, :] = get_R_cor_sq(get_lin_features_from_vertices(vertices, center_coords, vertices_features, R_arr[R_i]), main_mode)
+		R_cor[R_i, :, :] = get_R_cor_sq(get_lin_features_from_vertices(vertices, center_coords, vertices_features, R_arr[R_i]))
 		#print('R cor done: ' + str((R_i + 1) / N_R * 100) + ' %')
 
 	### ================== draw protein ======================
 	if(to_draw_centers or to_draw_atoms or to_draw_vertices):
 		fig_protein, ax_protein = my.get_fig('x (nm)', 'y (nm)', title='centers', projection='3d', zlbl='z (nm)')
 		if(to_draw_centers):
-			#for center_type_i in range(N_center_types):
-	# 			ax_protein.plot3D(center_coords_residues[center_type_i, :, 0], center_coords_residues[center_type_i, :, 1], center_coords_residues[center_type_i, :, 2], \
-	# 					 label='C:' + center_coords_names[center_type_i])
-	 		ax_protein.plot3D(center_coords[main_mode, :, 0], center_coords[main_mode, :, 1], center_coords[main_mode, :, 2], \
+	 		ax_protein.plot3D(center_coords[:, 0], center_coords[:, 1], center_coords[:, 2], \
 		                      label='C:' + center_coords_names[main_mode])
 		if(to_draw_atoms):
 			ax_protein.scatter3D(molecule.xyz[0, :, 0], molecule.xyz[0, :, 1], molecule.xyz[0, :, 2], \
-			      label='A:' + center_coords_names[center_type_i], s=1)
+			      label='A:' + center_coords_names[main_mode], s=1)
 		if(to_draw_vertices):
 			ax_protein.plot_trisurf(regular_trimesh.vertices[:,0], regular_trimesh.vertices[:,1], regular_trimesh.vertices[:,2], \
 						            triangles=regular_trimesh.faces, alpha=0.5)
@@ -215,11 +208,15 @@ def main():
 			for section_id_i in range(len(id_sections_to_draw)):
 				section_id = id_sections_to_draw[section_id_i]
 				section_title = 'section ' + str(section_id)
-				ax_protein.plot(center_coords[main_mode, section_id, 0], center_coords[main_mode, section_id, 1], center_coords[main_mode, section_id, 2], \
+				ax_protein.plot(center_coords[section_id, 0], center_coords[section_id, 1], center_coords[section_id, 2], \
 					               '.', markersize=10, color=my.colors[section_id_i], label=None)
 
+				center_2Dcoord_augmented = np.linalg.solve(sections_to_3D[section_id, :, :], \
+												         np.append(center_coords[section_id, :], 1).T)
 				if(to_draw_2D_sections):
 					fig_section, ax_section = my.get_fig("x'", "y'", title=section_title)
+					ax_section.scatter(center_2Dcoord_augmented[0], center_2Dcoord_augmented[1], \
+					                   s=10, color='red', label=('center' if subsection_id == 0 else None))
 
 				for subsection_id in range(len(sections_3D[section_id])):
 					subsection = sections_3D[section_id][subsection_id]
@@ -227,12 +224,8 @@ def main():
 					               '+', markersize=4, label=(section_title if subsection_id == 0 else None), color=my.colors[section_id_i])
 
 					if(to_draw_2D_sections):
-						center_2Dcoord_augmented = np.linalg.solve(sections_to_3D[main_mode, section_id, :, :], \
-														         np.append(center_coords[main_mode, section_id, :], 1).T)
 						ax_section.plot(sections_2D[section_id][subsection_id][0, :], sections_2D[section_id][subsection_id][1, :], \
 					                    '.-', markersize=2, label='subsections ' + str(subsection_id))
-						ax_section.scatter(center_2Dcoord_augmented[0], center_2Dcoord_augmented[1], \
-						                   s=10, color='red', label=('center' if subsection_id == 0 else None))
 
 				if(to_draw_2D_sections):
 					fig_section.legend()
@@ -251,8 +244,7 @@ def main():
 	if(to_plot_features):
 		fig_feat, ax_feat = my.get_fig('residue #', 'feature', title='features along the "' + center_coords_names[main_mode] + '"; $\sigma_{avg} = ' + str(Ravg) + '$')
 		for f_i in range(N_feat):
-			#ax_feat.plot(sq_nums, lin_feauture_sq_u[main_mode, :, f_i], '--o', markersize=3, label=features_names[f_i])
-			ax_feat.scatter((np.arange(N_centers) + 1) * (N_resd / N_centers), lin_feauture_sq_u[main_mode, :, f_i], s=2, label=features_names[f_i])
+			ax_feat.scatter((np.arange(N_centers) + 1) * (N_resd / N_centers), lin_feauture_sq_u[:, f_i], s=2, label=features_names[f_i])
 		ax_feat.plot([0, N_resd], [0, 0], label=None)
 		fig_feat.legend()
 
